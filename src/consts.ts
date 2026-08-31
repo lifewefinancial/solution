2aece90071d7b73d76734a0605306d74f8d2a314
lifewefinancial <lifewefinancial@gmail.com>
2026-08-31 10:14:12 -0400

    test mult accout:

--- src/consts.ts
+++ src/consts.ts
@@ -1,5 +1,5 @@
 // Place any global data in this file.
 // You can import this data from anywhere in your site by using the `import` keyword.
 
-export const SITE_TITLE = "Astro Blog";
-export const SITE_DESCRIPTION = "Welcome to my website!";
+export const SITE_TITLE = "桔子财富咨询有限公司";
+export const SITE_DESCRIPTION = "Welcome to 桔子财富咨询有限公司!";
